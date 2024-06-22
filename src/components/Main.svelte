a203ae120870357b2dca14e8f02eb83e81084726
calc1f4r <20yashs09@gmail.com>
2024-06-22 19:15:47 +0530

    Initial Commit

---
+++ src/components/Main.svelte
@@ -0,0 +1,43 @@
+<main class="flex flex-col flex-1 p-4">
+	<section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+		<div class="flex flex-col justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+			<h2 class="font-semibold text-3xl sm:text-5xl md:text-6xl">
+				Hi! I'm <span class="poppins text-violet-400">Yash</span> 👋 <br /> Full Stack & Solidity
+				<span class="poppins text-violet-400">Developer</span>
+				& Security Auditor
+			</h2>
+			<p class="text-lg sm:text-xl md:text-xl">
+				I'm a Sophomore focused on auditing and securing dApps currently on EVM-based Chains .
+			</p>
+			<p class=" -mt-5 text-base md:text-lg lg:text-xl font-medium">
+				In my free time, I'm all about diving into different chains
+				<span class="text-green-400"> AO, ICP, Sol ?</span>
+			</p>
+		</div>
+		<div class="relative shadow-2xl grid place-items-center">
+			<div class="p-3 border-2 border-violet-400 rounded-full">
+				<img
+					src="images/photo.png"
+					alt="Profile Image"
+					class="z-2 max-h-8vh w-72 h-72 rounded-full"
+					aria-hidden="true"
+				/>
+			</div>
+		</div>
+	</section>
+	<!-- <section id="projects" class="py-28 lg:py-32 flex flex-col gap-24">
+		<div class="flex flex-col gap-2 text-center">
+			<h6 class="text-large sm:text-xl md:text-2xl">A few of my Recent projects</h6>
+			<h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+				Curious to <span class="poppins text-violet-400">see</span> my work?
+			</h3>
+			<a
+				href="/"
+				target="_blank"
+				class="ms-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+			>
+				<i class="fa-regular fa-circle-play"></i>
+			</a>
+		</div>
+	</section> -->
+</main>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import '../app.css';
+	import Header from '../components/Header.svelte';
+	import Footer from '../components/Footer.svelte';
+	let y;
+	let innerHeight;
+	let innerWidth;
+	function goTop() {
+		document.body.scrollIntoView();
+	}
+</script>
+
+<div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
+	<div
+		class={'fixed bottom-0 w-full duration-200 p-10 z-[10]' +
+			(y > 0 ? ' opacity-full pointer-events-auto' : ' opacity-0 pointer-events-none')}
+	>
+		<button
+			on:click={goTop}
+			class="ml-auto bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer"
+		>
+			<i class="fa-solid fa-arrow-up grid place-items-center aspect-square"></i>
+		</button>
+	</div>
+	<Header {y} />
+	<slot />
+	<Footer />
+</div>
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
